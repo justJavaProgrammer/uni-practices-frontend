29ef5d25af9449f3e5ef46539de0a764a97c29e0
odeyalo <theprogrammerinfuture@gmail.com>
2026-03-21 15:53:56 +0200

    Set up lab03-js directory and connect main.js to all HTML pages

---
+++ lab03-js/js/main.js
@@ -0,0 +1,23 @@
+// main.js
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  console.log('DOM fully loaded and parsed');
+  
+  initActiveNav();
+  // Future initializations will be added here
+  // initMenuToggle();
+  // initThemeToggle();
+  // initBackToTop();
+  // initAccordion();
+  // initFilters();
+  // initModal();
+  // initContactForm();
+}
+
+/**
+ * Task 2: Highlight active page in navigation
+ */
+function initActiveNav() {
+    // To be implemented in Task 2.1
+}
